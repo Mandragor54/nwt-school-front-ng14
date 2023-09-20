70bedae087077c00b84c8dd7a676eba6d9d4cbc8
Mandragor <marie.trunkenwald@gmail.com>
2023-09-20 10:18:15 +0200

    "Fini"

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Person } from '../shared/types/person.type';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
