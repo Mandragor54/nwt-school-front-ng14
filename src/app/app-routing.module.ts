10d1a54e3f09878b4f66cb0d4865933a20f2d051
Mandragor <marie.trunkenwald@gmail.com>
2023-09-20 09:00:39 +0200

    Fini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // APP COMPONENTS
-// import { HomeComponent } ...
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // TODO
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
